2c22f041996d20bb928c7bb4928f5391c318f27c
elvin <ants.guoxf@gmail.com>
2022-05-07 09:53:08 +0800

    time utils

---
+++ utils/time.go
@@ -0,0 +1,12 @@
+package utils
+
+import "time"
+
+func PareTime(str string) time.Time {
+	date, _ := time.Parse("2006-01-02 15:04:05", str)
+	return date
+}
+
+func FormatTime(date time.Time) string {
+	return date.Format("2006-01-02 15:04:05")
+}
